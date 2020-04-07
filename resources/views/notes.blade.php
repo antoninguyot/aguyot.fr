3cfcad10730c9f4bb2c121f4d0838c656dcbee79
Antonin <antonin.guyot@icloud.com>
2020-04-07 20:25:04 +0200

    relevé s3 et passage sur cloudfront

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -6,7 +6,8 @@
         Si vous avez besoin d'accéder à mes résultats, voici ceux de ma première année de DUT Réseaux & Télécoms :
     </p>
     <ul>
-        <li><a href="/images/ReleveDUTS1.jpg">Relevé du 1er semestre</a></li>
-        <li><a href="/images/ReleveDUTS2.jpg">Relevé du 2ème semestre</a></li>
+        <li><a href="https://static.ateros.fr/Sc59MXJQL2pW7UryZu1kNIotTjmnRC.pdf">Relevé du 1er semestre</a></li>
+        <li><a href="https://static.ateros.fr/jFgYLRZODJQMo1mq8nP0U5wvTitxI6.pdf">Relevé du 2ème semestre</a></li>
+        <li><a href="https://static.ateros.fr/XSckKhUiWZPqD7xg4HjOf93s1wQBEI.pdf">Relevé du 3ème semestre</a></li>
     </ul>
 @endsection
